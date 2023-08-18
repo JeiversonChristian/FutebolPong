3f85578dbee410f3384c465f7838e8d7d74d7873
JeiversonChristian <jeiversonc@gmail.com>
2023-08-17 21:54:42 -0300

    adicionando campo, jogadores e bola

--- main.py
+++ main.py
@@ -3,8 +3,19 @@ import pygame
 pygame.init()
 
 window = pygame.display.set_mode([1360,700])
-
 title = pygame.display.set_caption("Futebol Pong")
+
+field = pygame.image.load("assets/field.png")
+window.blit(field, (0,0))
+
+player1 = pygame.image.load("assets/player1.png")
+window.blit(player1, (50, 280))
+
+player2 = pygame.image.load("assets/player2.png")
+window.blit(player2, (1235, 280))
+
+ball = pygame.image.load("assets/ball.png")
+window.blit(ball, (657, 327))
 
 loop = True
 while loop == True:
