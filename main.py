beba5e38b51e155a28209c8e28fe4738be77a298
JeiversonChristian <jeiversonc@gmail.com>
2023-08-17 21:58:17 -0300

    colocando as funções de desenhar dentro de uma função própria

--- main.py
+++ main.py
@@ -6,16 +6,16 @@ window = pygame.display.set_mode([1360,700])
 title = pygame.display.set_caption("Futebol Pong")
 
 field = pygame.image.load("assets/field.png")
-window.blit(field, (0,0))
-
 player1 = pygame.image.load("assets/player1.png")
-window.blit(player1, (50, 280))
-
 player2 = pygame.image.load("assets/player2.png")
-window.blit(player2, (1235, 280))
-
 ball = pygame.image.load("assets/ball.png")
-window.blit(ball, (657, 327))
+
+
+def draw():
+    window.blit(field, (0,0))
+    window.blit(player1, (50, 280))
+    window.blit(player2, (1235, 280))
+    window.blit(ball, (657, 327))
 
 loop = True
 while loop == True:
@@ -23,5 +23,7 @@ while loop == True:
     for events in pygame.event.get():
         if events.type == pygame.QUIT:
             loop = False
+
+    draw()
 
     pygame.display.update()
