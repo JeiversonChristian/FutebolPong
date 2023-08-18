9d54fda52bf96ff37c6593e78da493007831c8a1
JeiversonChristian <jeiversonc@gmail.com>
2023-08-17 22:07:03 -0300

    trocando const pos y dos players por variáveis

--- main.py
+++ main.py
@@ -6,8 +6,12 @@ window = pygame.display.set_mode([1360,700])
 title = pygame.display.set_caption("Futebol Pong")
 
 field = pygame.image.load("assets/field.png")
+
 player1 = pygame.image.load("assets/player1.png")
+player1_y = 280
+
 player2 = pygame.image.load("assets/player2.png")
+player2_y = 280
 
 ball = pygame.image.load("assets/ball.png")
 ball_x = 657
@@ -21,8 +25,8 @@ def move_ball():
 
 def draw():
     window.blit(field, (0,0))
-    window.blit(player1, (50, 280))
-    window.blit(player2, (1235, 280))
+    window.blit(player1, (50, player1_y))
+    window.blit(player2, (1235, player2_y))
     window.blit(ball, (ball_x, ball_y))
 
 loop = True
